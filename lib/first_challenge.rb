20eb41c2a49348f99687e7e9605aef5ddaf352be
Jason Gomez <gmzjsn@gmail.com>
2019-03-25 00:49:00 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,13 @@ def first_challenge
   }
 
   #your code here
+  contacts.map do |name, data|
+    data.map do |attribute, value|
+      if attribute == :favorite_icecream_flavors && name == "Freddy Mercury"
+        value.shift
+      end
+    end
+  end
 
 
   #remember to return your newly altered contacts hash!
