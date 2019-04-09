0398c64e13e76071daafed77fa2a71d96c5674ae
Carly Pecora <carly@carlypecora.com>
2019-04-09 17:39:46 -0400

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,15 @@ def first_challenge
 
   #your code here
 
-
+  contacts.each do |person, data|
+    if person == "Freddy Mercury"
+      data.each do |key, value|
+        if key == :favorite_icecream_flavors
+        value.shift
+        end
+      end
+    end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
