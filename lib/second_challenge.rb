0d62dc02ecb0ba1ca4eda99c3b71ae347d1cb200
Won Kim <wonsae.kim@gmail.com>
2019-04-15 03:37:25 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,13 @@ def second_challenge
   }
 
   #code your solution here!
+  food_list = []
+  groceries.each do |cats, food|
+    food.each do |name|
+      food_list << name
+    end
+  end
 
   
-
+  food_list
 end
