0810bdd5e0f4a324fc2d86228814d6c75c9305b8
Jennifer Oh <ohjennifer.90@gmail.com>
2019-04-15 13:39:27 -0400

    manipulating-hashes

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,6 +16,7 @@ def first_challenge
   #your code here
     contacts.each do |person, data|
         data.each do |attribute, value|
+    #contacts["Freddu Mercury"].each do |attribute, value|
            if attribute == :favorite_icecream_flavors
                value.delete_if do |flavor|
                flavor == "strawberry"
