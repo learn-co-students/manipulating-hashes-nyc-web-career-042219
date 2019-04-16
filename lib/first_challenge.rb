0432b2529b5cd736644c77507a48f107f19e02b7
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-16 04:10:46 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,13 +14,12 @@ def first_challenge
   }
 
   #your code here
-  
-  contacts.each do |attribute, value|
-    if attribute  == :favorite_icecream_flavors
-      value.delete("strawberry")
-        contacts
-        end
-      
-    end
 
+contacts.each do |person, data|
+data.each do |attribute, value|
+  if attribute == :favorite_icecream_flavors
+    value.delete_if {|flavor| flavor == "strawberry"}
+end
+end
+end
 end
